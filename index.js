596b8eae8b6aa05ceb26e2500006be1c235a8a13
Abhishek Yadav <103202897+Abhishek-Yadav-17@users.noreply.github.com>
2022-11-28 22:56:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+// identify Buttons
+
+var drumBtn= document.querySelectorAll(".drum");
+
+for(var i=0; i<drumBtn.length; i++){
+
+  drumBtn[i].addEventListener("click", function btnAudio(){
+   var btnInnerHTML= this.innerHTML;
+   makeSound(btnInnerHTML);
+   addAnimation(btnInnerHTML);
+ })
+}
+
+// assign keys to buttons
+
+document.addEventListener("keydown", function(event){
+  var drumKey= event.key;
+  makeSound(drumKey);
+  addAnimation(drumKey);
+})
+
+// Assign sound to buttons
+
+function makeSound(key){
+  switch(key){
+    case "w": var tom1= new Audio("sounds/tom-1.mp3");
+              tom1.play();
+              break;
+    case "a": var tom2= new Audio("sounds/tom-2.mp3");
+              tom2.play();
+              break;
+    case "s": var tom3= new Audio("sounds/tom-3.mp3")
+              tom3.play();
+              break;
+    case "d": var tom4= new Audio("sounds/tom-4.mp3");
+              tom4.play();
+              break;
+    case "j": var snare= new Audio("sounds/snare.mp3");
+              snare.play();
+              break;
+    case "k": var crash= new Audio("sounds/crash.mp3");
+              crash.play();
+              break;
+    case "l": var kick= new Audio("sounds/kick-bass.mp3");
+              kick.play();
+              break;
+    default: console.log("Refresh the page");
+
+  }
+}
+
+//Assign Animation to Buttons
+
+function addAnimation(currentKey){
+  var activeBtn= document.querySelector("."+currentKey)
+  activeBtn.classList.add("pressed");
+
+  setTimeout(function(){activeBtn.classList.remove("pressed"), 150});
+}
